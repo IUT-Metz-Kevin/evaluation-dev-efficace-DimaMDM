40e79d41c586e80b2bbaca88980069427a91d6aa
Dimitri Makogon <dimitri.makogon@gmail.com>
2025-06-03 13:56:46 +0200

    GREEN

--- test/demineur.ts
+++ test/demineur.ts
@@ -1,5 +1,20 @@
 import { assertEquals } from "jsr:@std/assert";
 
+export class Demineur {
+  grille: string[][];
+
+  constructor(grille: string[][]) {
+    this.grille = grille;
+  }
+
+  resoudre(): string {
+    if (this.grille.length === 1 && this.grille[0].length === 1 && this.grille[0][0] === ".") {
+      return "0";
+    }
+    return "";
+  }
+}
+
 Deno.test("case vide 0", () => {
   const demineur = new Demineur([["."]]);
   assertEquals(demineur.resoudre(), "0");
